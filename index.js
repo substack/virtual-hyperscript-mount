1f29b827549f0c45e08df32bc1d1de2c67b74667
James Halliday <mail@substack.net>
2016-02-14 01:12:10 -0800

    fallback when Set isn't available

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 module.exports = function () {
-  var elems = new Set
-  return function (fn) {
-    return function (elem) {
-      if (!elems.has(elem)) {
-        elems.add(elem)
-        fn(elem)
+  if (typeof Set !== 'undefined') {
+    var elems = new Set
+    return function (fn) {
+      return function (elem) {
+        if (!elems.has(elem)) {
+          elems.add(elem)
+          fn(elem)
+        }
+      }
+    }
+  } else {
+    var elems = []
+    return function (fn) {
+      return function (elem) {
+        if (elems.indexOf(elem) < 0) {
+          elems.push(elem)
+          fn(elem)
+        }
       }
     }
   }
